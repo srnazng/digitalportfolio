4e49f872e77f0d8b8c1d6bf6d670effc70ad6901
srnazng <srnazng@gmail.com>
2020-07-28 11:59:55 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Typography } from '@material-ui/core';
 import './App.css';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles({
+  root: {
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    border: 0,
+    borderRadius: 3,
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    color: 'white',
+    height: '800',
+    padding: '0 30px',
+  },
+  heading: {
+    textAlign: 'center',
+  }
+});
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <Typography variant="h1" className={classes.heading}>
+        MTPSHacks
+      </Typography>
     </div>
   );
 }
