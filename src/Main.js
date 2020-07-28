a6e2c5880deae64b4b005ba68358bd07e445c14d
Ria Vora <riavora@rias-mbp.myfiosgateway.com>
2020-07-28 19:47:29 -0400

    Sponsor Page

--- src/Main.js
+++ src/Main.js
@@ -1,65 +1,92 @@
-import React from 'react';
-import { Link as RouterLink } from 'react-router-dom';
-import { Typography } from '@material-ui/core';
-import './App.css';
-import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import MTPSLogo from './MTPSLogo.png';
-import Submissions from './Submissions.js';
-import { Grid } from '@material-ui/core';
-import ButtonGroup from '@material-ui/core/ButtonGroup';
-import Sponsors from './Sponsors.js'
+import React from "react";
+import { Link as RouterLink } from "react-router-dom";
+import { Typography } from "@material-ui/core";
+import "./App.css";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import MTPSLogo from "./MTPSLogo.png";
+import Submissions from "./Submissions.js";
+import { Grid } from "@material-ui/core";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+import Sponsors from "./Sponsors.js";
 
 const useStyles = makeStyles({
-    root: {
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-        color: 'white',
-        height: '800',
-        paddingTop: "10%",
-        paddingBottom: "40%",
-    },
-    heading: {
-        textAlign: 'center',
-    },
-    image: {
-        alignItems: 'center',
-        width: '200px',
-        paddingLeft: "35%",
-    },
-    menu: {
-        alignItems: 'center',
-        paddingLeft: "25%",
-        color: 'white',
-    },
-    button: {
-        color: 'white',
-    },
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+    color: "white",
+    height: "800",
+    paddingTop: "10%",
+    paddingBottom: "40%",
+  },
+  heading: {
+    textAlign: "center",
+  },
+  image: {
+    alignItems: "center",
+    width: "600px",
+    paddingLeft: "0%",
+  },
+  menu: {
+    alignItems: "center",
+    paddingLeft: "25%",
+    color: "white",
+  },
+  button: {
+    color: "white",
+  },
 });
 
 function Main() {
-    const classes = useStyles();
-    return (
-        <div className={classes.root}>
-            <Grid>
-                <Grid item xs="12" spacing="1">
-                    <Typography variant="h1" className={classes.heading}>
-                        MTPSHacks
-        </Typography>
-                    <img src={MTPSLogo} className={classes.image} alt="Logo" justify="center" />
-                </Grid>
-                <Grid item xs="12">
-                    <ButtonGroup className={classes.menu} variant="text" aria-label="text primary button group">
-                        <Button className={classes.button} to="/About" component={RouterLink} >About</Button>
-                        <Button className={classes.button} to="/Submissions" component={RouterLink}>Submissions</Button>
-                        <Button className={classes.button} to="/Sponsors" component={RouterLink}>Sponsors</Button>
-                    </ButtonGroup>
-                </Grid>
-            </Grid>
-        </div>
-    );
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <Grid>
+        <Grid item xs="12" spacing="1">
+          <Typography variant="h1" className={classes.heading}>
+            MTPSHacks
+          </Typography>
+          <img
+            src={MTPSLogo}
+            className={classes.image}
+            alt="Logo"
+            justify="center"
+          />
+        </Grid>
+        <Grid item xs="12">
+          <ButtonGroup
+            className={classes.menu}
+            variant="text"
+            aria-label="text primary button group"
+          >
+            <Button
+              className={classes.button}
+              to="/About"
+              component={RouterLink}
+            >
+              About
+            </Button>
+            <Button
+              className={classes.button}
+              to="/Submissions"
+              component={RouterLink}
+            >
+              Submissions
+            </Button>
+            <Button
+              className={classes.button}
+              to="/Sponsors"
+              component={RouterLink}
+            >
+              Sponsors
+            </Button>
+          </ButtonGroup>
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
 
 export default Main;
--- src/Sponsors.js
+++ src/Sponsors.js
@@ -1,68 +1,91 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Link as RouterLink } from 'react-router-dom';
-import { Typography } from '@material-ui/core';
-import './App.css';
-import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import MTPSLogo from './MTPSLogo.png';
-import Submissions from './Submissions.js';
-import { Grid } from '@material-ui/core';
-import ButtonGroup from '@material-ui/core/ButtonGroup';
-import IconButton from '@material-ui/core/IconButton';
+import React from "react";
+import logo from "./logo.svg";
+import { Link as RouterLink } from "react-router-dom";
+import { Typography } from "@material-ui/core";
+import "./App.css";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import MTPSLogo from "./MTPSLogo.png";
+import JANJ from "./janj.png";
+import thinkKindness from "./thinkKindness.png";
+import igniteStem from "./igniteSTEM.png";
+import adp from "./adp.png";
+import verizon from "./verizon.png";
+import jj from "./jj.png";
+import Submissions from "./Submissions.js";
+import { Grid } from "@material-ui/core";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+import IconButton from "@material-ui/core/IconButton";
 
 const useStyles = makeStyles({
-    root: {
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-        color: 'white',
-        height: '800',
-        paddingTop: "10%",
-        paddingBottom: "40%",
-    },
-    heading: {
-        textAlign: 'center',
-    },
-    image: {
-        alignItems: 'center',
-        width: '100px',
-        position: 'absolute',
-        top: '20px',
-        left: '20px',
-    },
-    menu: {
-        alignItems: 'center',
-        paddingLeft: "22%",
-        color: 'white',
-    },
-    button: {
-        color: 'white',
-    },
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    background: "white",
+    color: "black",
+    height: "800",
+    paddingTop: "10%",
+    paddingBottom: "40%",
+  },
+  heading: {
+    textAlign: "center",
+  },
+  image: {
+    alignItems: "center",
+    width: "100px",
+    position: "absolute",
+    top: "20px",
+    left: "20px",
+  },
+  menu: {
+    alignItems: "center",
+    paddingLeft: "22%",
+    color: "black",
+  },
+  button: {
+    color: "black",
+  },
+  logo: {
+    width: "1000px",
+    paddingTop: "10px",
+    paddingBottom: "10px",
+  },
 });
 
 function Sponsors() {
-    const classes = useStyles();
-    return (
-        <div className={classes.root}>
-            <IconButton to="/" component={RouterLink} className={classes.image}>
-                <img src={MTPSLogo} alt="Logo" justify="center" className={classes.image} />
-            </IconButton>
-            <Grid>
-                <Grid item xs="12" spacing="1">
-                    <Typography variant="h1" className={classes.heading}>
-                        Sponsors
-                    </Typography>
-                </Grid>
-                <Grid item xs="12">
-                    <Typography variant="body1">
-                        We could do have this hackathon without the support of our sponsors! Thank you!
-                    </Typography>
-                </Grid>
-            </Grid>
-        </div>
-    );
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <IconButton to="/" component={RouterLink} className={classes.image}>
+        <img
+          src={MTPSLogo}
+          alt="Logo"
+          justify="center"
+          className={classes.image}
+        />
+      </IconButton>
+      <Grid>
+        <Grid item xs="12" spacing="1">
+          <Typography variant="h1" className={classes.heading}>
+            Sponsors
+          </Typography>
+        </Grid>
+        <Grid item xs="12">
+          <Typography variant="body1">
+            We could do have this hackathon without the support of our sponsors!
+            Thank you!
+          </Typography>
+        </Grid>
+      </Grid>
+      <img src={JANJ} className={classes.logo} />
+      <img src={igniteStem} className={classes.logo} />
+      <img src={thinkKindness} className={classes.logo} />
+      <img src={adp} className={classes.logo} />
+      <img src={verizon} className={classes.logo} />
+      <img src={jj} className={classes.logo} />
+    </div>
+  );
 }
 
 export default Sponsors;
