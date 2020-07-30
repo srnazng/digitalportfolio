7564e761231b1c321adf39d487bbb0d67e1da036
Ria Vora <riavora@rias-mbp.myfiosgateway.com>
2020-07-30 13:38:09 -0400

    Formatting Submissions

--- src/Card.js
+++ src/Card.js
@@ -1,127 +1,139 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import clsx from 'clsx';
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import Collapse from '@material-ui/core/Collapse';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import { red } from '@material-ui/core/colors';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import Link from '@material-ui/core/Link';
-import SummerLauren from './projectMedia/SummerLauren.mp4';
-import ReactPlayer from 'react-player';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import clsx from "clsx";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import Collapse from "@material-ui/core/Collapse";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import { red } from "@material-ui/core/colors";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import Link from "@material-ui/core/Link";
+import SummerLauren from "./projectMedia/SummerLauren.mp4";
+import ReactPlayer from "react-player";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        margin: "30px",
-    },
-    media: {
-        height: 0,
-        paddingTop: '56.25%', // 16:9
-
-    },
-    expand: {
-        transform: 'rotate(0deg)',
-        marginLeft: 'auto',
-        transition: theme.transitions.create('transform', {
-            duration: theme.transitions.duration.shortest,
-        }),
-    },
-    expandOpen: {
-        transform: 'rotate(180deg)',
-    },
-    media: {
-        marginTop: "0px",
-        paddingTop: "0px",
-        contentAlign: "center",
-        justify: "center",
-    }
+  root: {
+    margin: "30px",
+    transitionDuration: "0.3s",
+    display: "flex",
+    justifyContent: "space-between",
+    flexDirection: "column",
+    height: "100%",
+  },
+  media: {
+    height: 0,
+    paddingTop: "56.25%", // 16:9
+  },
+  expand: {
+    transform: "rotate(0deg)",
+    marginLeft: "auto",
+    transition: theme.transitions.create("transform", {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: "rotate(180deg)",
+  },
+  media: {
+    marginTop: "0px",
+    paddingTop: "0px",
+    contentAlign: "center",
+    justify: "center",
+  },
 }));
 
 export default function SubmissionCard(props) {
-    const classes = useStyles();
-    const [expanded, setExpanded] = React.useState(false);
-    const preventDefault = (event) => event.preventDefault();
+  const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
+  const preventDefault = (event) => event.preventDefault();
 
-    const handleExpandClick = () => {
-        setExpanded(!expanded);
-    };
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
 
-    function showMedia(url) {
-        console.log(url);
-        return (
-            <div width={1} className={classes.media}>
-                <ReactPlayer url={SummerLauren} playing width="400px" justify="center" />
-            </div>);
-    }
-
+  function showMedia(url) {
+    console.log(url);
     return (
-        <Card className={classes.root}>
-            <CardHeader
-                title={props.projectName}
-                subheader={props.memberNames}
-            />
-            <CardContent>
-                {props.projectName == "Summer and Lauren's Social Good Website" ? showMedia("SummerLauren") : ""}
-                <Typography variant="body2" color="textSecondary" component="p">
-                    {props.description}
-                </Typography>
-            </CardContent>
-            <CardActions disableSpacing>
-                <IconButton
-                    className={clsx(classes.expand, {
-                        [classes.expandOpen]: expanded,
-                    })}
-                    onClick={handleExpandClick}
-                    aria-expanded={expanded}
-                    aria-label="show more"
-                >
-                    <ExpandMoreIcon />
-                </IconButton>
-            </CardActions>
-            <Collapse in={expanded} timeout="auto" unmountOnExit>
-                <CardContent>
-                    <Typography>What inspired your project idea?</Typography>
-                    <Typography variant="body2" color="textSecondary" component="p">
-                        {props.inspiration}
-                        <br /><br />
-                    </Typography>
-                    <Typography>What challenges did your team run into?</Typography>
-                    <Typography variant="body2" color="textSecondary" component="p">
-                        {props.challenges}
-                        <br /><br />
-                    </Typography>
-                    <Typography>What accompmlishments are you proud of?</Typography>
-                    <Typography variant="body2" color="textSecondary" component="p">
-                        {props.accomplishments}
-                        <br /><br />
-                    </Typography>
-                    <Typography>What did you learn?</Typography>
-                    <Typography variant="body2" color="textSecondary" component="p">
-                        {props.learn}
-                        <br /><br />
-                    </Typography>
-                    <Typography>What are the next steps for you and your team?</Typography>
-                    <Typography variant="body2" color="textSecondary" component="p">
-                        {props.nextSteps}
-                        <br /><br />
-                    </Typography>
-                    <Typography>Links</Typography>
-                    <Typography variant="body2" color="textSecondary" component="p">
-                        <Link href={props.link1} >
-                            {props.link1}
-                        </Link>
-                        <br />
-                        <Link href={props.link2}>
-                            {props.link2}
-                        </Link>
-                    </Typography>
-                </CardContent>
-            </Collapse>
-        </Card>
+      <div width={1} className={classes.media}>
+        <ReactPlayer
+          url={SummerLauren}
+          playing
+          width="400px"
+          justify="center"
+        />
+      </div>
     );
+  }
+
+  return (
+    <Card className={classes.root}>
+      <CardHeader title={props.projectName} subheader={props.memberNames} />
+      <CardContent>
+        {props.projectName == "BruhSummer and Lauren's Social Good Website"
+          ? showMedia("SummerLauren")
+          : ""}
+        <Typography variant="body2" color="textSecondary" component="p">
+          {props.description}
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton
+          className={clsx(classes.expand, {
+            [classes.expandOpen]: expanded,
+          })}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </IconButton>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography>What inspired your project idea?</Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {props.inspiration}
+            <br />
+            <br />
+          </Typography>
+          <Typography>What challenges did your team run into?</Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {props.challenges}
+            <br />
+            <br />
+          </Typography>
+          <Typography>What accompmlishments are you proud of?</Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {props.accomplishments}
+            <br />
+            <br />
+          </Typography>
+          <Typography>What did you learn?</Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {props.learn}
+            <br />
+            <br />
+          </Typography>
+          <Typography>
+            What are the next steps for you and your team?
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {props.nextSteps}
+            <br />
+            <br />
+          </Typography>
+          <Typography>Links</Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            <Link href={props.link1}>{props.link1}</Link>
+            <br />
+            <Link href={props.link2}>{props.link2}</Link>
+          </Typography>
+        </CardContent>
+      </Collapse>
+    </Card>
+  );
 }
--- src/Submissions.js
+++ src/Submissions.js
@@ -1,75 +1,76 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import { Link as RouterLink } from 'react-router-dom';
-import { Typography } from '@material-ui/core';
-import './App.css';
-import { makeStyles } from '@material-ui/core/styles';
-import IconButton from '@material-ui/core/IconButton';
-import MTPSLogo from './MTPSLogo.png';
-import { Grid } from '@material-ui/core';
-import SubmissionList from './SubmissionList'
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import { Link as RouterLink } from "react-router-dom";
+import { Typography } from "@material-ui/core";
+import "./App.css";
+import { makeStyles } from "@material-ui/core/styles";
+import IconButton from "@material-ui/core/IconButton";
+import MTPSLogo from "./MTPSLogo.png";
+import { Grid } from "@material-ui/core";
+import SubmissionList from "./SubmissionList";
 
 const useStyles = makeStyles({
-    root: {
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-        color: 'white',
-        height: '800',
-        paddingTop: "100px",
-        paddingBottom: "30px",
-    },
-    image: {
-        alignItems: 'center',
-        width: '100px',
-        position: 'absolute',
-        top: '20px',
-        left: '20px',
-    },
-    heading: {
-        textAlign: 'center',
-    },
-    text: {
-        alignItems: 'center',
-        textAlign: 'center',
-
-    },
-    button: {
-        color: 'white',
-    },
-    card: {
-        width: '800px',
-    }
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+    color: "white",
+    height: "800",
+    paddingTop: "100px",
+    paddingBottom: "30px",
+  },
+  image: {
+    alignItems: "center",
+    width: "100px",
+    position: "absolute",
+    top: "20px",
+    left: "20px",
+  },
+  heading: {
+    textAlign: "center",
+  },
+  text: {
+    alignItems: "center",
+    textAlign: "center",
+  },
+  button: {
+    color: "white",
+  },
+  card: {
+    width: "800px",
+  },
 });
 
 function Submissions() {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
-        <div className={classes.root} >
-            <IconButton to="/" component={RouterLink} className={classes.image}>
-                <img src={MTPSLogo} alt="Logo" justify="center" className={classes.image} />
-            </IconButton>
-            <Grid >
-                <Grid item xs="12" spacing="1">
-                    <Typography variant="h1" className={classes.heading}>
-                        Submissions
-                    </Typography>
-                </Grid>
-                <Grid item xs="12">
-                    <Typography variant="body1" justify="center" className={classes.text}>
-                        Teams of students submitted their project for either the Social Good Challenge or the Marketing Challenge!
-                    </Typography>
-                </Grid>
-                <Grid item xs="12" justify="center" className={classes.card} >
-                    <SubmissionList />
-                </Grid>
-            </Grid>
-
-        </div>
-    );
-
+  return (
+    <div className={classes.root}>
+      <IconButton to="/" component={RouterLink} className={classes.image}>
+        <img
+          src={MTPSLogo}
+          alt="Logo"
+          justify="center"
+          className={classes.image}
+        />
+      </IconButton>
+      <Grid>
+        <Grid item xs="12" spacing="1">
+          <Typography variant="h1" className={classes.heading}>
+            Submissions
+          </Typography>
+        </Grid>
+        <Grid item xs="12">
+          <Typography variant="body1" justify="center" className={classes.text}>
+            Teams of students submitted their project for either the Social Good
+            Challenge or the Marketing Challenge!
+          </Typography>
+        </Grid>
+      </Grid>
+      <SubmissionList />
+    </div>
+  );
 }
 
 export default Submissions;
