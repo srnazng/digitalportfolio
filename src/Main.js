17646f27668f64421c79e70ffb5442e784bd5036
Ria Vora <riavora@rias-mbp.myfiosgateway.com>
2020-08-03 12:33:35 -0400

    git deploy test

--- src/Main.js
+++ src/Main.js
@@ -71,7 +71,7 @@ function Main() {
         <Grid className={classes.body}>
           <Grid item xs="12" spacing="1">
             <Typography variant="h1" className={classes.heading}>
-              MTPSHacks
+              MTPSHacks hi 
           </Typography>
           </Grid>
           <Grid item xs="12" className={classes.menu}>
