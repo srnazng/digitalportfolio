ae2e1883fcc6bf16c30c1cceab58344f8aae9797
Ria Vora <riavora@rias-mbp.myfiosgateway.com>
2020-08-03 12:50:48 -0400

    dialog box fix

--- src/Card.js
+++ src/Card.js
@@ -14,6 +14,8 @@ import Dialog from "@material-ui/core/Dialog";
 import MuiDialogTitle from "@material-ui/core/DialogTitle";
 import MuiDialogContent from "@material-ui/core/DialogContent";
 import MuiDialogActions from "@material-ui/core/DialogActions";
+import IconButton from "@material-ui/core/IconButton";
+import CloseIcon from "@material-ui/icons/Close";
 import { sizing } from '@material-ui/system';
 
 const useStyles = makeStyles((theme) => ({
